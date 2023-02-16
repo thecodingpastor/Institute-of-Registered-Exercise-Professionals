574e1868dbfa2eead8d374aa56efca11092c72a3
Ayeni Michael <ayenimike80@gmail.com>
2023-02-17 00:06:18 +0100

    Added not found page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Link from "next/link";
+import Router from "next/router";
+import classes from "./Error.module.scss";
 
 const Error = () => {
-  return <div>Error in the front end</div>;
+  return (
+    <div className={classes.Container}>
+      Page Not Found.<span onClick={() => Router.back()}>Go back</span> or{" "}
+      <Link href="/">Go Home</Link>
+    </div>
+  );
 };
 
 export default Error;

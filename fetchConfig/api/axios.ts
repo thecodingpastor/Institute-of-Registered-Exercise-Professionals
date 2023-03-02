0691cf7c54645b6b32ea3a22cbcddbf0738a6946
Ayeni Michael <ayenimike80@gmail.com>
2023-03-02 22:12:53 +0100

    logging base url

--- fetchConfig/api/axios.ts
+++ fetchConfig/api/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3000/api";
+console.log(BASE_URL);
 
 export default axios.create({
   baseURL: BASE_URL,

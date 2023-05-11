24bcb3dc96d7372fb6d0a2b0ebb39a36c4827bfb
Ayeni Michael <ayenimike80@gmail.com>
2023-05-11 08:33:39 +0100

    Created envValues.js as process.env is not working in aws

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const envValues = require("./envValues");
 
 module.exports = (phase) => {
+  console.log("NODE_ENV PROD", process.env.NODE_ENV);
+  console.log("env values ========>>>>>>> ", envValues?.EMAIL_SERVICE);
   return {
+    env: envValues,
     images: {
       domains: ["res.cloudinary.com"],
     },

32d646b29148dc0e2d8afb3596e8dc72972482fe
Ayeni Michael <ayenimike80@gmail.com>
2023-05-24 08:19:57 +0100

    changed recaptcha keys

--- components/layout/CaptchaContainer.tsx
+++ components/layout/CaptchaContainer.tsx
@@ -4,6 +4,7 @@ const CaptchaContainer = ({ children }) => {
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey="6LcR1bAlAAAAAIsXhV-YNt1O9SvX0pDbmYr-v_XB"
+      // reCaptchaKey="6LfbfwQmAAAAALZRVekmUeoDO28eVY1JRnZX9nrU"
       scriptProps={{
         async: false,
         defer: false,

f55781333c94b7808177712803f1b5fd5e209aa1
Ayeni Michael <ayenimike80@gmail.com>
2023-05-24 08:15:18 +0100

    changed recaptcha keys

--- components/layout/CaptchaContainer.tsx
+++ components/layout/CaptchaContainer.tsx
@@ -3,8 +3,7 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 const CaptchaContainer = ({ children }) => {
   return (
     <GoogleReCaptchaProvider
-      // reCaptchaKey="6LeRI9ckAAAAACRBtZYCWDmlvXofaAnmYypkAgYM"
-      reCaptchaKey="6LfbfwQmAAAAALZRVekmUeoDO28eVY1JRnZX9nrU"
+      reCaptchaKey="6LcR1bAlAAAAAIsXhV-YNt1O9SvX0pDbmYr-v_XB"
       scriptProps={{
         async: false,
         defer: false,
